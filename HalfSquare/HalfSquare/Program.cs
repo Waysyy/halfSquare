91f1a3d63b64a8620f16ed01cfc157b7298dc2bf
Waysyy <annatkach225@gmail.com>
2022-09-22 12:17:25 +0700

    .

---
+++ HalfSquare/HalfSquare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HalfSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
